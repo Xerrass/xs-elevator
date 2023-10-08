800a7c58159a142cb1ee179b35e3b47bb116187e
Jonathan <jonathan@mohs.works>
2023-10-09 01:11:33 +0200

    Update functions.ts
    Cleanup and some Comments

--- server/functions.ts
+++ server/functions.ts
@@ -22,7 +22,10 @@ export class ElevatorFunctions {
                     isPlayerOnly: _isPlayerOnly,
                     range: _range,
                     async callback(player) {
+                        // Check if Stops have certain items or permissions to be accessed
                         for (let _stop of elevator.elevatorStops) {
+
+                            // TODO maybe needs a Fix as now the KEy Item takes Precedence and the permission is not checked
                             if (_stop.keyItemName) {
                                 if (await Athena.player.inventory.has(player, _stop.keyItemName, 1)) {
                                     _stop.show = true
@@ -38,17 +41,23 @@ export class ElevatorFunctions {
                             } else {
                                 _stop.show = true
                             }
+
+                            // Dont show stop if it is the starting Point
                             if (_stop.name === stop.name) {
                                 _stop.show = false
                             }
-                            if (Athena.player.permission.hasAccountPermission(player, 'debug')) {
+
+                            // If Account/Character has permission "debug" it will show all stops no matter what
+                            if (Athena.player.permission.hasAccountPermission(player, 'debug') || Athena.player.permission.hasPermission(player, 'debug')) {
                                 _stop.show = true
                             }
 
                         }
-                        alt.emitClient(player, ELEVATOR_EVENTS.VIEW_OPEN, elevator)
+                        alt.emitClient(player, ELEVATOR_EVENTS.VIEW_OPEN, elevator);
                     },
                 })
+
+                // Checks if the showmarker atribute is set and if not sets it to show the marker otherwise uses whatever is set in config of Elevator
                 let showmarker = typeof stop.showmarker !== "undefined" ? stop.showmarker : (stop.showmarker = true)
                 if (showmarker) {
                     Athena.controllers.marker.append({
@@ -65,13 +74,13 @@ export class ElevatorFunctions {
     static teleport(player: alt.Player, targetCords: alt.IVector3, targetRot: alt.Vector3 = new alt.Vector3(0, 0, 0)) {
         Athena.player.emit.fadeScreenToBlack(player, 1000)
         alt.setTimeout(() => {
-            Athena.player.safe.setPosition(player, targetCords.x, targetCords.y, targetCords.z)
+            Athena.player.safe.setPosition(player, targetCords.x, targetCords.y, targetCords.z);
             player.rot = targetRot
             alt.setTimeout(() => {
-                Athena.player.emit.fadeScreenFromBlack(player, 500)
+                Athena.player.emit.fadeScreenFromBlack(player, 500);
             }, 100)
         }, 1100)
-        //Athena.player.safe.set
     }
 }
-alt.onClient(ELEVATOR_EVENTS.TELEPORT, ElevatorFunctions.teleport)
+
+alt.onClient(ELEVATOR_EVENTS.TELEPORT, ElevatorFunctions.teleport);
