047f826faa2a05433294e8d4bee67c7976f56f3f
Jonathan <jonathan@mohs.works>
2023-10-09 04:41:55 +0200

    Update config.ts
    more excamples

--- shared/config.ts
+++ shared/config.ts
@@ -11,11 +11,28 @@ elevators.push({
         {
             name: "Hub",
             pos: new alt.Vector3(-164.8, -153.8, 93.7),
-
+            rot: new alt.Vector3(0, 0, -1.72),
         },
         {
             name: "Ground Level",
             pos: new alt.Vector3(-237.08, -256.26, 39.117),
+            showmarker: false
+        }
+    ]
+})
+
+elevators.push({
+    name: "Rockford Plaza",
+    isCarElevator: true,
+    elevatorStops: [
+        {
+            name: "Hub",
+            pos: new alt.Vector3(-166.21978759765625, -161.06373596191406, 93.6966552734375),
+            rot: new alt.Vector3(0, 0, 1),
+        },
+        {
+            name: "Ground Level",
+            pos: new alt.Vector3(-166.87911987304688, -170.62417602539062, 93.6966552734375),
             showmarker: false
         }
     ]
